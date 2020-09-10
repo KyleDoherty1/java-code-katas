455cb76d639989579cb6858c8324ac1f2f58d1c5
Kyle Doherty <kyledoherty1@gmail.com>
2020-09-11 00:22:45 +0100

    Added Accum Code Kata

--- src/main/java/com/kyledoherty/codekatas/CodekatasApplication.java
+++ src/main/java/com/kyledoherty/codekatas/CodekatasApplication.java
@@ -3,11 +3,16 @@ package com.kyledoherty.codekatas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static com.kyledoherty.codekatas.Accumul.accum;
+
 @SpringBootApplication
 public class CodekatasApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CodekatasApplication.class, args);
+
+        System.out.println(accum("ZpglnRxqenU"));
     }
 
 }
---
+++ src/test/java/com/kyledoherty/codekatas/AccumulTest.java
@@ -0,0 +1,21 @@
+package com.kyledoherty.codekatas;
+
+import org.junit.jupiter.api.Test;
+
+import static com.kyledoherty.codekatas.Accumul.accum;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AccumulTest {
+
+    @Test
+    public void testAccum() {
+        assertEquals(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+        assertEquals(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+        assertEquals(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+        assertEquals(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+        assertEquals(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+
+    }
+
+
+}
