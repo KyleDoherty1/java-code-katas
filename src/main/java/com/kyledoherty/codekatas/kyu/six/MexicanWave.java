2511a00a44437395dc5b6200955abea658d537e6
Kyle Doherty <kyledoherty1@gmail.com>
2020-09-17 20:44:55 +0100

    Added interesting solution.
    note the continue to do 'nothing' instead of doing 'something'

--- src/main/java/com/kyledoherty/codekatas/kyu/six/MexicanWave.java
+++ src/main/java/com/kyledoherty/codekatas/kyu/six/MexicanWave.java
@@ -1,7 +1,5 @@
 package com.kyledoherty.codekatas.kyu.six;
 
-import java.util.Arrays;
-
 //https://www.codewars.com/kata/58f5c63f1e26ecda7e000029/train/java
 public class MexicanWave {
 
@@ -9,20 +7,30 @@ public class MexicanWave {
         //Set array MexicanWave with length of string minus any whitespaces
         String[] mexicanWave = new String[str.replace(" ", "").length()];
 
-        Arrays.fill(mexicanWave, str);
-
         int charPointer = 0;
         for (int i = 0; i < mexicanWave.length; i++) {
-            char[] currentWord = mexicanWave[i].toCharArray();
+            char[] currentWord = str.toCharArray();
+
             while(currentWord[charPointer] == ' ')
                 charPointer++;
 
             currentWord[charPointer] = Character.toUpperCase(currentWord[charPointer]);
             mexicanWave[i] = new String(currentWord);
             charPointer++;
-
         }
         return mexicanWave;
     }
+
+    /*
+    public static String[] wave(String str) {
+      List<String> list = new ArrayList<>();
+      for (int i = 0; i < str.length(); i++) {
+        char ch = str.charAt(i);
+        if (ch == ' ') continue;
+        list.add(str.substring(0,i) + Character.toUpperCase(ch) + str.substring(i+1));
+      }
+      return list.toArray(new String[0]);
+    }
+     */
 
 }
