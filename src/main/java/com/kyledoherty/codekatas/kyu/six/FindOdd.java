f11c0bd63b5875b94c07d633685e21c3105b7ab1
Kyle Doherty <kyledoherty1@gmail.com>
2020-09-18 20:02:18 +0100

    Solved FineOne Kata

--- src/main/java/com/kyledoherty/codekatas/kyu/six/FindOdd.java
+++ src/main/java/com/kyledoherty/codekatas/kyu/six/FindOdd.java
@@ -1,9 +1,26 @@
 package com.kyledoherty.codekatas.kyu.six;
 
+import java.util.HashMap;
+
+//https://www.codewars.com/kata/54da5a58ea159efa38000836/train/java
 public class FindOdd {
 
-    public static int findIt(int[] a) {
-        return odd;
+    public static int findIt(int[] sequence) {
+
+        HashMap<Integer, Integer> intOccurences = new HashMap<Integer, Integer>();
+
+        for(int i: sequence){
+            if(!(intOccurences.containsKey(i)))
+                intOccurences.put(i, 1);
+            else
+                intOccurences.put(i, intOccurences.get(i)+1);
+        }
+
+        for(HashMap.Entry<Integer, Integer> entry : intOccurences.entrySet())
+            if(entry.getValue() % 2 == 1)
+                return entry.getKey();
+
+        return 0;
     }
 
 }
