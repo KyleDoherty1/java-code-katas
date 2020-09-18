40b6fd042a2cfdab357bc0dd4da001980a36afee
Kyle Doherty <kyledoherty1@gmail.com>
2020-09-18 20:16:53 +0100

    Solved Vowel kata

--- src/main/java/com/kyledoherty/codekatas/kyu/seven/Vowels.java
+++ src/main/java/com/kyledoherty/codekatas/kyu/seven/Vowels.java
@@ -1,4 +1,15 @@
 package com.kyledoherty.codekatas.kyu.seven;
 
 public class Vowels {
+
+    public static int getCount(String str) {
+        int vowelsCount = 0;
+
+        for (char c : str.toCharArray()) {
+            if(c == 'a'|| c == 'e' || c == 'i'|| c == 'o' || c == 'u')
+                vowelsCount++;
+        }
+        return vowelsCount;
+    }
+
 }
--- src/test/java/com/kyledoherty/codekatas/kyu/seven/VowelsTest.java
+++ src/test/java/com/kyledoherty/codekatas/kyu/seven/VowelsTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class VowelsTest {
     @Test
     public void testCase1() {
-        assertEquals("Nope!", 5, Vowels.getCount("abracadabra"));
+        assertEquals(5, Vowels.getCount("abracadabra"));
     }
 
 }
