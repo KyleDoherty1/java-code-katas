b29acc8c5b3077da717b7f220c44335f1ff34e94
Kyle Doherty <kyledoherty1@gmail.com>
2020-09-21 20:10:27 +0100

    Added Troll kata
    Time to solve!

---
+++ src/main/java/com/kyledoherty/codekatas/kyu/seven/Troll.java
@@ -0,0 +1,14 @@
+package com.kyledoherty.codekatas.kyu.seven;
+
+//https://www.codewars.com/kata/52fba66badcd10859f00097e/train/java
+public class Troll {
+
+    public static String disemvowel(String str) {
+        StringBuilder answer = new StringBuilder();
+
+        
+
+        return answer.toString();
+    }
+
+}
